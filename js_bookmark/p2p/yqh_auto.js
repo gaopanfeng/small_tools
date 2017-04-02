0eab504102984b8db3a262cc16dc51f33432b281
gaopanfeng <gaopanfeng1987@126.com>
2017-04-02 16:45:41 +0800

    Create yqh_auto.js
    一起好

---
+++ js_bookmark/p2p/yqh_auto.js
@@ -0,0 +1,41 @@
+/**  一起好
+auto invest
+*/
+//javascript:
+(function(){
+	var $btn_shouxin = $('.avcredit input');
+	var $btn_max = $('.money>a');
+	var $btn_agree = $('.detail-bid form label>input');
+	var $btn_submit = $('.detail-bid form button[type=submit]');
+	function run(){
+		if(!$btn_shouxin.prop("checked")){
+			$('.avcredit input').click();
+		}
+		$btn_max.click();
+		if(!$btn_agree.prop('checked')){
+			$btn_agree.click();
+		}
+		$btn_submit.click();
+	}
+	function loop(func,timeout){
+		setTimeout(function(){
+			var ret = func
+			if(ret){
+				loop(func,timeout);
+			}
+		},timeout);
+		
+	}
+	function check(){
+		if($btn_submit.text() == '立即抢购'){
+			return true;
+		}
+	}
+	loop(function(){
+		if(check()){
+			run();
+			return false;
+		}
+		return true;
+	},100);
+})();
