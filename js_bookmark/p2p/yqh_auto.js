5674fd0f3336a4fe3e58338efcb13e660d872bb1
gaopanfeng <gaopanfeng1987@126.com>
2017-05-03 16:02:24 +0800

    yiqihao

--- js_bookmark/p2p/yqh_auto.js
+++ js_bookmark/p2p/yqh_auto.js
@@ -5,23 +5,29 @@ auto invest
 (function(){
 	var $btn_shouxin = $('.avcredit input');
 	var $btn_max = $('.money>a');
-	var $btn_agree = $('.detail-bid form label>input');
+	var $btn_agree = $('.detail-bid form>label>input');
 	var $btn_submit = $('.detail-bid form button[type=submit]');
+	var $btn_number = $('.detail-bid form input[type=number]').eq(0);
 	function run(){
-		if(!$btn_shouxin.prop("checked")){
-			$('.avcredit input').click();
+		while(!$btn_shouxin.prop("checked")){
+			$btn_shouxin.click();
 		}
+
 		$btn_max.click();
+		$btn_number.focus();
+		if($btn_number.val() < 10000){
+			alert("金额小于10000");
+			return;
+		}
 		
 		$btn_agree.prop('disabled',false);
-		$btn_agree.click();
+		while(!$btn_agree.prop("checked")){
+			$btn_agree.click();
+		}
 		
 		$btn_submit.prop("disabled",false);
 		$btn_submit.click();
-		setTimeout(function(){
-			$btn_submit.click();
-		},1000)
-	};
+	}
 	function loop(func,timeout){
 		console.log(new Date());
 		setTimeout(function(){
@@ -31,15 +37,23 @@ auto invest
 			}
 		},timeout);
 		
-	};
+	}
 	function check(){
 		if($btn_submit.text().trim() == '立即抢购'){
 			return true;
 		}
-	};
+	}
+	function runtimeout(timeout){
+		setTimeout(function(){
+			run();
+		},timeout);
+	}
 	loop(function(){
 		if(check()){
 			run();
+			for(var i=1;i<=10;i++){
+				runtimeout(1000*i);	
+			}
 			return false;
 		}
 		return true;
