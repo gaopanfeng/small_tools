9d0189f1ea85ec381f41f20a96620e47377e6ad2
gaopanfeng <gaopanfeng@sogou-inc.com>
2018-05-10 14:23:41 +0800

    玖融bbs

--- js_bookmark/p2p/jiurong_bbs2.js
+++ js_bookmark/p2p/jiurong_bbs2.js
@@ -2,8 +2,8 @@
 //javascript:
 (function(){
     function run(){
-        var url = location.href;
-        var w = window.open(url);
+      var url = location.href;
+      var w = window.open(url);
         var closenum = 0;
         w.onunload = function(){
             closenum++;
@@ -12,18 +12,35 @@
                 setTimeout(run,50000);
             }
         };
-        setTimeout(function() {
-            w.scrollTo(0,10000);
-            w.$('fastpostmessage').value = '签到签到';
-            var hash = w.document.getElementsByName('sechash')[0].value;
-            w.updateseccode(hash);
-            w.$('seccode'+hash+'_menu').style.display='block';
-            var a = w.prompt("验证码","");
-            if(a){
-                w.$('seccodeverify_'+hash).value=a;
-                w.$('fastpostsubmit').click();
+        var times = 0;
+        var ing = false;
+        var handle = setInterval(function() {
+            if(ing){
+                return;
             }
-        }, 3000);
+            if(times > 20){
+                clearInterval(handle);
+            }
+            ing = true;
+            times++;
+            try{
+                w.scrollTo(0,10000);
+                w.$('fastpostmessage').value = '签到签到';
+                var hash = w.document.getElementsByName('sechash')[0].value;
+                w.updateseccode(hash);
+                w.$('seccode'+hash+'_menu').style.display='block';
+                var a = w.prompt("验证码","");
+                if(a){
+                    w.$('seccodeverify_'+hash).value=a;
+                    w.$('fastpostsubmit').click();
+                }
+                clearInterval(handle);
+            }catch(e){
+
+            }finally{
+                ing = false;
+            }
+        }, 1000);
     }
     run();
 })();
