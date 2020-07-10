0932481939de446e6db07d65c662d135673840ce
gaopanfeng <gaopanfeng@sogou-inc.com>
2020-07-10 09:52:13 +0800

    fix: 微信阅读去头

---
+++ js_bookmark/wread.js
@@ -0,0 +1,6 @@
+// javascript:
+(function () {
+    document.querySelector('.readerTopBar').style.display = 'none';
+})();
+
+// javascript:(function () {    document.querySelector('.readerTopBar').style.display = 'none'})();
